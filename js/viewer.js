8f58a7af8526472760ae99579cb98f9d7f12f674
lanery <ryanilane@gmail.com>
2020-05-21 16:31:20 +0200

    working serving from raw githubusercontent

--- js/viewer.js
+++ js/viewer.js
@@ -8,7 +8,7 @@ var viewer = OpenSeadragon({
         minLevel: 0,
         maxLevel: 6,
         getTileUrl: function( level, x, y ){
-          return "http://sonic/long_term_storage/rlane/CATMAID/projects/20200507_RL012/lil_EM_montaged/6/" + y + "_" + x + "_" + (6-level) + ".png"
+          return "https://raw.githubusercontent.com/lanery/iCAT-seadragon/master/projects/20200507_RL012/EM/" + y + "_" + x + "_" + (6-level) + ".png"
         }
     }
 });
