9af870eb6681276efbbc6173c31182ea7ae580a7
lanery <ryanilane@gmail.com>
2020-05-25 16:32:19 +0200

    adding sequenceMode

--- js/viewer.js
+++ js/viewer.js
@@ -1,14 +1,25 @@
 var viewer = OpenSeadragon({
     id: "seadragon-viewer",
     prefixUrl: "https://openseadragon.github.io/openseadragon/images/",
-    tileSources:   {
-        height: 43123,
-        width:  37301,
-        tileSize: 1024,
-        minLevel: 0,
-        maxLevel: 6,
-        getTileUrl: function( level, x, y ){
-          return "https://raw.githubusercontent.com/lanery/iCAT-MAID-data/master/20200507_RL012/EM/" + y + "_" + x + "_" + (6-level) + ".png"
-        }
-    }
+    showNavigator: true,
+    tileSources:   [{
+      height: 43123,
+      width:  37301,
+      tileSize: 1024,
+      minLevel: 0,
+      maxLevel: 6,
+      getTileUrl: function( level, x, y ){
+        return "https://raw.githubusercontent.com/lanery/iCAT-MAID-data/master/20200507_RL012/EM/" + y + "_" + x + "_" + (6-level) + ".png"}
+      },{
+      height: 43123,
+      width:  37301,
+      tileSize: 1024,
+      minLevel: 0,
+      maxLevel: 6,
+      getTileUrl: function( level, x, y ){
+        return "https://raw.githubusercontent.com/lanery/iCAT-MAID-data/master/20200507_RL012/EM/" + y + "_" + x + "_" + (6-level) + ".png"}
+      },
+    ],
+    sequenceMode: true,
+    showReferenceStrip: true,
 });
