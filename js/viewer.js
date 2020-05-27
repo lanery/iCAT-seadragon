23c799771f6a1976e67542825813ea469fe8c64b
lanery <ryanilane@gmail.com>
2020-05-27 11:54:05 +0200

    added multiple section support (manual)

--- js/viewer.js
+++ js/viewer.js
@@ -9,7 +9,7 @@ var viewer = OpenSeadragon({
       minLevel: 0,
       maxLevel: 6,
       getTileUrl: function( level, x, y ){
-        return "https://raw.githubusercontent.com/lanery/iCAT-MAID-data/master/20200507_RL012/EM/" + y + "_" + x + "_" + (6-level) + ".png"}
+        return "https://raw.githubusercontent.com/lanery/iCAT-MAID-data/master/projects/20200507_RL012/0/" + y + "_" + x + "_" + (6-level) + ".png"}
       },{
       height: 43123,
       width:  37301,
@@ -17,8 +17,56 @@ var viewer = OpenSeadragon({
       minLevel: 0,
       maxLevel: 6,
       getTileUrl: function( level, x, y ){
-        return "https://raw.githubusercontent.com/lanery/iCAT-MAID-data/master/20200507_RL012/EM/" + y + "_" + x + "_" + (6-level) + ".png"}
-      },
+        return "https://raw.githubusercontent.com/lanery/iCAT-MAID-data/master/projects/20200507_RL012/1/" + y + "_" + x + "_" + (6-level) + ".png"}
+      },{
+      height: 43123,
+      width:  37301,
+      tileSize: 1024,
+      minLevel: 0,
+      maxLevel: 6,
+      getTileUrl: function( level, x, y ){
+        return "https://raw.githubusercontent.com/lanery/iCAT-MAID-data/master/projects/20200507_RL012/2/" + y + "_" + x + "_" + (6-level) + ".png"}
+      },{
+      height: 43123,
+      width:  37301,
+      tileSize: 1024,
+      minLevel: 0,
+      maxLevel: 6,
+      getTileUrl: function( level, x, y ){
+        return "https://raw.githubusercontent.com/lanery/iCAT-MAID-data/master/projects/20200507_RL012/3/" + y + "_" + x + "_" + (6-level) + ".png"}
+      },{
+      height: 43123,
+      width:  37301,
+      tileSize: 1024,
+      minLevel: 0,
+      maxLevel: 6,
+      getTileUrl: function( level, x, y ){
+        return "https://raw.githubusercontent.com/lanery/iCAT-MAID-data/master/projects/20200507_RL012/4/" + y + "_" + x + "_" + (6-level) + ".png"}
+      },{
+      height: 43123,
+      width:  37301,
+      tileSize: 1024,
+      minLevel: 0,
+      maxLevel: 6,
+      getTileUrl: function( level, x, y ){
+        return "https://raw.githubusercontent.com/lanery/iCAT-MAID-data/master/projects/20200507_RL012/5/" + y + "_" + x + "_" + (6-level) + ".png"}
+      },{
+      height: 43123,
+      width:  37301,
+      tileSize: 1024,
+      minLevel: 0,
+      maxLevel: 6,
+      getTileUrl: function( level, x, y ){
+        return "https://raw.githubusercontent.com/lanery/iCAT-MAID-data/master/projects/20200507_RL012/6/" + y + "_" + x + "_" + (6-level) + ".png"}
+      },{
+      height: 43123,
+      width:  37301,
+      tileSize: 1024,
+      minLevel: 0,
+      maxLevel: 6,
+      getTileUrl: function( level, x, y ){
+        return "https://raw.githubusercontent.com/lanery/iCAT-MAID-data/master/projects/20200507_RL012/7/" + y + "_" + x + "_" + (6-level) + ".png"}
+      }
     ],
     sequenceMode: true,
     showReferenceStrip: true,
